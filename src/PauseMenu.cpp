c968449dd6df4304ce1fd2552d11c36102510d2e
Chris Thrasher <chrisjthrasher@gmail.com>
2022-12-15 23:55:28 -0700

    Fix pause menu text

--- src/PauseMenu.cpp
+++ src/PauseMenu.cpp
@@ -1,4 +1,5 @@
 #include "PauseMenu.hpp"
+#include "Cell.hpp"
 #include "ResourceManager.hpp"
 
 #include <SFML/Graphics/Font.hpp>
@@ -7,10 +8,11 @@ PauseMenu::PauseMenu()
 {
     m_text.setFont(get_font());
     m_text.setString("Game Paused");
-    m_text.setCharacterSize(24);
+    m_text.setCharacterSize(48);
 
-    // FIXME: Center in the window
-    m_text.setPosition({ 200, 200 });
+    m_text.setPosition(sf::Vector2f(9u * Cell::size) / 2.f);
+    m_text.setOrigin({ m_text.getLocalBounds().left + m_text.getLocalBounds().width / 2,
+                       m_text.getLocalBounds().top + m_text.getLocalBounds().height / 2 });
 }
 
 auto PauseMenu::update(const sf::Event& event) -> std::variant<bool, std::unique_ptr<State>>
