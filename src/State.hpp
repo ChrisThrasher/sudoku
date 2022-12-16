1a3742b0191d45a91f6cd407120f168cb8ad5daf
Chris Thrasher <chrisjthrasher@gmail.com>
2022-12-15 23:20:40 -0700

    Fix clang-tidy warnings

--- src/State.hpp
+++ src/State.hpp
@@ -9,7 +9,12 @@
 
 class State : public sf::Drawable {
 public:
-    virtual ~State() = default;
+    State() = default;
+    ~State() override = default;
+    State(const State&) = default;
+    State& operator=(const State&) = default;
+    State(State&&) noexcept = default;
+    State& operator=(State&&) noexcept = default;
 
     [[nodiscard]] virtual auto update(const sf::Event& event) -> std::variant<bool, std::unique_ptr<State>> = 0;
 };
